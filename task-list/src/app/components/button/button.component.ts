18ad29f77e7757c4d468e43f356ef819a106d544
cecilialsantin <chikhakituti@gmail.com>
2022-09-11 21:24:01 -0300

    primer boton

---
+++ task-list/src/app/components/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string = "";
+  @Input() color: string = "";
+  @Output() btnClick = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.btnClick.emit();
+  }
+
+}
